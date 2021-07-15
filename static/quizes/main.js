03b2fae53def88ff838571145e8ecf3268a17a28
PelaNisha <nishapela777@gmail.com>
2021-07-15 21:43:10 +0545

    committed

--- static/quizes/main.js
+++ static/quizes/main.js
@@ -5,5 +5,18 @@ const modalBody = document.getElementById('modal-body-confirm')
 
 modalBtns.forEach(modalBtn=>modalBtn.addEventListener('click',()=>{
     const pk = modalBtn.getAttribute('data-pk')
-    console.log(pk)
+    
+    const name = modalBtn.getAttribute('data-quiz')
+    const numQuestions = modalBtn.getAttribute('data-questions')
+    const difficulty = modalBtn.getAttribute('data-difficulty')
+    const  scoreToPass= modalBtn.getAttribute('data-pass')
+    const time = modalBtn.getAttribute('data-time')
+
+    modalBody.innerHTML = "<li>Subject:" + name +'<br/>'+"<li>Number of questions: "
+        +numQuestions+'<br/><li>Difficulty Level:'+difficulty+'<br/><li>Score to Pass '
+        +scoreToPass+'<br/><li>Time:'+ time
+    
+    
+
+        
 }))
